50d8dc537085d40bb9b90034db432b0ae49836a1
vishwaspatil <vsp@narola.email>
2016-06-27 17:49:07 +0530

    chapter 7 is complete

---
+++ app/controllers/demo_controller.rb
@@ -0,0 +1,18 @@
+class DemoController < ApplicationController
+  layout false
+  def index
+  	
+  end
+  def hello
+  	#render('index')
+  	@array = [1,2,3,4,5]
+  	@id = params['id']
+  	@page = params[:page]
+  end
+  def other_hello
+  	redirect_to(:Controller => 'demo' , :action => 'index')
+  end
+  def lynda
+  	redirect_to("http://google.com")
+  end
+end
