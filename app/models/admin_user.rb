92216bb82c985ebdb27c5c28cd918c8082eacd9b
vishwaspatil <vsp@narola.email>
2016-06-28 17:58:48 +0530

    chapter 8 is complete

---
+++ app/models/admin_user.rb
@@ -0,0 +1,5 @@
+class AdminUser < ActiveRecord::Base
+	has_and_belongs_to_many :pages
+
+	has_many :section_edits
+end
---
+++ app/models/subject.rb
@@ -0,0 +1,10 @@
+class Subject < ActiveRecord::Base
+	has_many :pages
+
+	scope :visible, lambda { where(:visible => true) }
+	scope :invisible, lambda { where(:visible => false) }
+	scope :sorted, lambda { order("subjects.position ASC") }
+	scope :search, lambda { |query|
+		where(["name LIKE ?", "%#{query}%"])
+	  }
+end
