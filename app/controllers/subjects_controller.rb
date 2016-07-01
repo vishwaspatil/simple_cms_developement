61eb67c99fb4ade1c4abe9bfa1fcd1fc66107b9e
vishwaspatil <vsp@narola.email>
2016-07-01 18:07:39 +0530

    chapter 11 is complete

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -1,3 +1,4 @@
+
 class SubjectsController < ApplicationController
   
   layout "admin"
@@ -12,6 +13,7 @@ class SubjectsController < ApplicationController
 
   def new
     @subject = Subject.new
+    @subject_count = Subject.count + 1
   end
 
   def create
@@ -20,12 +22,14 @@ class SubjectsController < ApplicationController
       flash[:notice]= "Subject created successfully"
       redirect_to(:action => 'index')
     else
+      @subject_count = Subject.count + 1
       render ('new')
     end
   end
 
   def edit
      @subject = Subject.find(params[:id])
+     @subject_count = Subject.count
   end
 
   def update
@@ -34,6 +38,7 @@ class SubjectsController < ApplicationController
       flash[:notice]= "Subject updated successfully"
       redirect_to(:action => 'show', :id => @subject.id)
     else
+      @subject_count = Subject.count
       render ('edit')
     end
   end
